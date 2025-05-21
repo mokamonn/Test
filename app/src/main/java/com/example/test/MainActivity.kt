e27c556f9ec9aafb0ab4447a73d38d5c9510c228
mokamonn <ak2488707@s.rku.ac.jp>
2025-05-21 17:50:06 +0900

    kozono:ログ追加

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log // ← これを追加！
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ここにログ出力を追加！
+        Log.d("MainActivity", "アプリが起動しました")
     }
 }
